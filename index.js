028c7c07fea18c8304d41d9a1d43da7f8ea10736
Leco Iglesias <leoalmig@gmail.com>
2018-01-25 17:36:07 -0200

    adiciona mais fortunes

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 
 const fortunes = [
   'Embedded!',
-  'IPhone barato somente nas lojas americanas'
+  'IPhone barato somente nas lojas americanas',
+  'O Lair cresceu muito profissionalmente',
+  'Em uma cidade com muitas motos Yamaha e só duas Honda, qual o nome do filme? Pocahondas!'
 ]
 
 const randomIndex = Math.floor(Math.random() * fortunes.length)
